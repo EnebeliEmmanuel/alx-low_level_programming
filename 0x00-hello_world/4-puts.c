a94e479333c7c530b66d0a4068b9ad6869d105cf
EnebeliEmmanuel <iemmaenebli@gmail.com>
2022-03-10 18:17:27 +0100

    cool

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-\**
+/**
  * main - main block
- * Description: print text using puts
  * Return: 0
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle\n");
-	Return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
