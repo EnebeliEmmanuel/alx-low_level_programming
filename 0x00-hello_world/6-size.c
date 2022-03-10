48159cd17c5ab4c96370a4324ded731ab8903408
EnebeliEmmanuel <iemmaenebli@gmail.com>
2022-03-10 18:19:47 +0100

    cool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - main function
- * Description: prints size of variable types
+ * main - main block
  * Return: 0
  */
 int main(void)
 {
-	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
