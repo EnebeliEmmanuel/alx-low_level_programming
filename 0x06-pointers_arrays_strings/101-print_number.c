038292aac4965c6f9036efcfe8b6646e50d9fe86
Elevatedpeace <Peace08182443958>
2022-03-24 21:55:46 +0100

    cool

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
 
 /**
- * print_number - prints an integer.
- * @n: integer
- * Return: void
+ * print_number - prints an integer;
+ * @n: integer to be printed;
  */
-
 void print_number(int n)
 {
 	unsigned int n1;
